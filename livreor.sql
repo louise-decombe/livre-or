54837875ba44082070ada61bba1ce35ef318ef79
louise-decombe <62133595+louise-decombe@users.noreply.github.com>
2020-06-01 17:16:17 +0200

    suppression des doublons

--- livreor.sql
+++ livreor.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2020 at 10:50 PM
+-- Generation Time: Jun 01, 2020 at 05:15 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
   `id_utilisateur` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=59 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=82 ;
 
 --
 -- Dumping data for table `commentaires`
@@ -40,8 +40,11 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
 
 INSERT INTO `commentaires` (`id`, `commentaire`, `id_utilisateur`, `date`) VALUES
 (56, 'Un repas agréable je reviendrais', 1, '0000-00-00 00:00:00'),
-(57, 'IntÃ©ressant comme lieu', 1, '2020-05-30 10:49:14'),
-(58, '', 1, '2020-05-30 10:49:16');
+(57, 'IntÃ©ressant comme lieu', 2, '2020-05-30 10:49:14'),
+(59, 'Le repas Ã©tait satisfaisant pour le prix.', 2, '2020-06-01 11:18:26'),
+(79, 'test', 0, '2020-06-01 04:58:19'),
+(80, 'ok', 0, '2020-06-01 05:02:14'),
+(81, 'coucou', 0, '2020-06-01 05:02:25');
 
 -- --------------------------------------------------------
 
@@ -54,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `utilisateurs`
@@ -63,7 +66,8 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
 (1, 'marine', '827ccb0eea8a706c4c34a16891f84e7b'),
 (2, 'laura', '827ccb0eea8a706c4c34a16891f84e7b'),
-(3, 'sarah', '827ccb0eea8a706c4c34a16891f84e7b');
+(3, 'sarah', '827ccb0eea8a706c4c34a16891f84e7b'),
+(4, 'patrick', '827ccb0eea8a706c4c34a16891f84e7b');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
